8b6bb2476bcc104e6977beb2089979691835bbab
Emanuel1522 <emanuel.rr1522@gmail.com>
2025-05-16 16:20:33 -0500

    Se arregla problema

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { enrutador } from './router/enrutador.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './Index.css'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
