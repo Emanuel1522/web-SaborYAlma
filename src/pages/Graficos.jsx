4979f3fdcd4e4b8bfd468b97e40f6c610f1fe689
Emanuel1522 <emanuel.rr1522@gmail.com>
2025-06-12 10:45:40 -0500

    imagenes

--- src/pages/Graficos.jsx
+++ src/pages/Graficos.jsx
@@ -23,14 +23,29 @@ const Graficos = () => {
         <div>
           <div className="menu-container">
             <div className="listaPlatos">
-                <div className="itemGraficos">
-                    <img src={"/graficos/uno.png"} alt="" />
-                </div>
+              <div className="itemGraficos">
+                <img src={"/graficos/uno.png"} alt="" />
+              </div>
             </div>
             <div className="listaPlatos">
-                <div className="itemGraficos">
-                    <img src={"/graficos/dos.png"} alt="" />
-                </div>
+              <div className="itemGraficos">
+                <img src={"/graficos/dos.png"} alt="" />
+              </div>
+            </div>
+            <div className="listaPlatos">
+              <div className="itemGraficos">
+                <img src={"/graficos/tres.png"} alt="" />
+              </div>
+            </div>
+            <div className="listaPlatos">
+              <div className="itemGraficos">
+                <img src={"/graficos/cuatro.png"} alt="" />
+              </div>
+            </div>
+            <div className="listaPlatos">
+              <div className="itemGraficos">
+                <img src={"/graficos/cinco.png"} alt="" />
+              </div>
             </div>
           </div>
         </div>
